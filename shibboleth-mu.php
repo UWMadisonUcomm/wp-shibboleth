9bcf1ddd0a0fc3a82f5e43d54b22137fe72045b6
Nick Weaver <jnweaver@wisc.edu>
2018-04-10 13:07:16 -0500

    Fix syntax error in shibboleth-mu.php

--- shibboleth-mu.php
+++ shibboleth-mu.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/shibboleth/shibboleth.php';
 
 
 function shibboleth_muplugins_loaded() {
-	add_filter('shibboleth_plugin_path', function('$p') {
+	add_filter('shibboleth_plugin_path', function($p) {
     return WPMU_PLUGIN_URL . "/shibboleth";
   });
 }
